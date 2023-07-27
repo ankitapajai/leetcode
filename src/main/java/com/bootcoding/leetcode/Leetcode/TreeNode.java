511cdb22083e4e2a2d053deb2a987d437dad0a45
ankitapajai <ankitapajai@gmail.com>
2023-07-27 20:40:06 +0530

    Node class..

---
+++ src/main/java/com/bootcoding/leetcode/Leetcode/TreeNode.java
@@ -0,0 +1,17 @@
+package com.bootcoding.leetcode.Leetcode;
+class TreeNode {
+    // Data stored in the tree node
+    int data; // Or any other data type based on your requirement
+    // References to child nodes
+    TreeNode left; // Reference to the left child node
+    TreeNode right; // Reference to the right child node
+
+    // Constructor to initialize the tree node with data
+    public TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
